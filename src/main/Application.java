c3520144e3cda9a90efdb3284c75db37ed9a285a
Timo Pagel <github@timo-pagel.de>
2023-11-06 16:35:35 +0100

    chore: add test

--- src/main/Application.java
+++ src/main/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+
 @SpringBootApplication
 public class Application {
 
