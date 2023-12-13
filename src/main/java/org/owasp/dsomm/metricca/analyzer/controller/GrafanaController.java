3ac87cf86498457fb0560976259a549c51ac104e
Timo Pagel <github@timo-pagel.de>
2023-12-13 17:14:00 +0100

    chore: Beautify

--- src/main/java/org/owasp/dsomm/metricca/analyzer/controller/GrafanaController.java
+++ src/main/java/org/owasp/dsomm/metricca/analyzer/controller/GrafanaController.java
@@ -2,7 +2,6 @@ package org.owasp.dsomm.metricca.analyzer.controller;
 
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.owasp.dsomm.metricca.analyzer.grafana.OverviewDashboard;
-import org.owasp.dsomm.metricca.analyzer.grafana.PanelConfiguration;
 import org.owasp.dsomm.metricca.analyzer.model.FlattenDate;
 import org.owasp.dsomm.metricca.analyzer.yaml.deserialization.Activity;
 import org.owasp.dsomm.metricca.analyzer.yaml.deserialization.Application;
@@ -43,8 +42,8 @@ public class GrafanaController {
 
   @RequestMapping(value = "/activity/{activityName}/nodate", method = RequestMethod.GET)
   @ResponseBody
-  public HashMap<String,Activity> getActivitiesSimpleNoDate(@PathVariable String activityName) throws Exception {
-    HashMap<String,Activity> activityMap = new HashMap<String,Activity>();
+  public HashMap<String, Activity> getActivitiesSimpleNoDate(@PathVariable String activityName) throws Exception {
+    HashMap<String, Activity> activityMap = new HashMap<String, Activity>();
     for (Application application : applicationDirector.getApplications()) {
       for (Activity activity : application.getActivities(activityName)) {
         if (activity.getName().equals(activityName)) {
--- src/main/java/org/owasp/dsomm/metricca/analyzer/controller/GrafanaDashboardController.java
+++ src/main/java/org/owasp/dsomm/metricca/analyzer/controller/GrafanaDashboardController.java
@@ -1,25 +1,17 @@
 package org.owasp.dsomm.metricca.analyzer.controller;
 
-import org.eclipse.jgit.api.errors.GitAPIException;
 import org.owasp.dsomm.metricca.analyzer.grafana.OverviewDashboard;
 import org.owasp.dsomm.metricca.analyzer.grafana.PanelConfiguration;
-import org.owasp.dsomm.metricca.analyzer.model.FlattenDate;
-import org.owasp.dsomm.metricca.analyzer.yaml.deserialization.Activity;
-import org.owasp.dsomm.metricca.analyzer.yaml.deserialization.Application;
 import org.owasp.dsomm.metricca.analyzer.yaml.deserialization.ApplicationDirector;
-import org.owasp.dsomm.metricca.analyzer.yaml.deserialization.components.DateComponent;
-import org.owasp.dsomm.metricca.analyzer.yaml.deserialization.components.DatePeriodComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
 
 @Controller
 public class GrafanaDashboardController {
@@ -30,7 +22,8 @@ public class GrafanaDashboardController {
 
   @Autowired
   private OverviewDashboard overviewDashboard;
-  @RequestMapping(value = "/teamdashboard", method = RequestMethod.GET, produces="application/json")
+
+  @RequestMapping(value = "/teamdashboard", method = RequestMethod.GET, produces = "application/json")
   @ResponseBody
   public String getTeamDashboard() throws Exception {
     PanelConfiguration panelConfiguration = new PanelConfiguration("Team Dashboard", "timeseries", "teamdashboard");
