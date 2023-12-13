5d1f13757d11abd76e9045afcb0d3b45ec3de5ec
Timo Pagel <github@timo-pagel.de>
2023-12-13 17:16:25 +0100

    chore: Beautify

--- src/main/java/org/owasp/dsomm/metricca/analyzer/grafana/OverviewDashboard.java
+++ src/main/java/org/owasp/dsomm/metricca/analyzer/grafana/OverviewDashboard.java
@@ -14,7 +14,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -24,7 +23,22 @@ import java.util.*;
 @Configuration
 public class OverviewDashboard {
   private static final Logger logger = LoggerFactory.getLogger(OverviewDashboard.class);
+  @Value("${metricCA.api.baseurl}")
+  private String apiBaseUrl;
+  @Value("${metricCA.grafana.template.path:src/main/resources/templates/}")
+  private String panelTemplatePath;
+  @Value("${metricCA.grafana.template.panelbasename:panel-infinity-}")
+  private String panelBaseName;
+  @Value("${metricCA.grafana.template.dashboardprefix:grafana-dashboard-}")
+  private String dashboardTemplatePrefix;
+  @Value("${metricCA.grafana.template.filepostfix:.ftl}")
+  private String templateFilePostfix;
+  @Value("${metricCA.grafana.datasource.uuid:overview:e407f775-8b38-4c67-b2c3-298086473afa}")
+  private String datasourceUuid;
+  private String dashboardType = "overview";
 
+  public OverviewDashboard() {
+  }
 
   @Bean
   public FreeMarkerViewResolver freemarkerViewResolver() {
@@ -42,32 +56,11 @@ public class OverviewDashboard {
     return freeMarkerConfigurer;
   }
 
-  @Value("${metricCA.api.baseurl}")
-  private String apiBaseUrl;
-
-  @Value("${metricCA.grafana.template.path:src/main/resources/templates/}")
-  private String panelTemplatePath;
-  @Value("${metricCA.grafana.template.panelbasename:panel-infinity-}")
-  private String panelBaseName;
-
-  @Value("${metricCA.grafana.template.dashboardprefix:grafana-dashboard-}")
-  private String dashboardTemplatePrefix;
-
-  @Value("${metricCA.grafana.template.filepostfix:.ftl}")
-  private String templateFilePostfix;
-
-  @Value("${metricCA.grafana.datasource.uuid:overview:e407f775-8b38-4c67-b2c3-298086473afa}")
-  private String datasourceUuid;
-
-  private String dashboardType = "overview";
-  public OverviewDashboard() {
-  }
-
   public String getDashboard(Collection<PanelConfiguration> panels) throws IOException, TemplateException {
     Template template1 = getTemplate(dashboardTemplatePrefix + dashboardType + templateFilePostfix);
     Map<String, Object> input = new HashMap<>();
     input.put("title", "Vogella example");
-    input.put("panelsAsString",  String.join(",", getPanels(panels)));
+    input.put("panelsAsString", String.join(",", getPanels(panels)));
     StringWriter stringWriter = new StringWriter();
     template1.process(input, stringWriter);
     return stringWriter.toString();
@@ -77,10 +70,10 @@ public class OverviewDashboard {
     FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
     freemarker.template.Configuration freeMarkerConfig = freeMarkerConfigurer.createConfiguration();
     FileTemplateLoader ftl1 = new FileTemplateLoader(new File(panelTemplatePath));
-    MultiTemplateLoader mtl = new MultiTemplateLoader(new TemplateLoader[] { ftl1 });
+    MultiTemplateLoader mtl = new MultiTemplateLoader(new TemplateLoader[]{ftl1});
 
     freeMarkerConfig.setTemplateLoader(mtl);
-    Template template1 =  freeMarkerConfig.getTemplate(templatePath);
+    Template template1 = freeMarkerConfig.getTemplate(templatePath);
     return template1;
   }
 
@@ -88,15 +81,16 @@ public class OverviewDashboard {
     Collection<String> panels = new ArrayList<>();
     for (PanelConfiguration panelConfiguration : panelConfigurations) {
       String panel = getPanel(panelConfiguration);
-      panels.add( panel);
+      panels.add(panel);
     }
-  return panels;
+    return panels;
   }
+
   private String getPanel(PanelConfiguration panelConfiguration) throws TemplateException, IOException {
-    Template template = getTemplate(panelBaseName +dashboardType+"/" +  panelConfiguration.getType() + templateFilePostfix);
+    Template template = getTemplate(panelBaseName + dashboardType + "/" + panelConfiguration.getType() + templateFilePostfix);
     Map<String, Object> input = new HashMap<>();
     input.put("title", panelConfiguration.getTitle());
-    input.put("apiUrl", apiBaseUrl + "/"+panelConfiguration.getUrl());
+    input.put("apiUrl", apiBaseUrl + "/" + panelConfiguration.getUrl());
     input.put("datasource-uuid", datasourceUuid);
 
     StringWriter stringWriter = new StringWriter();
@@ -119,6 +113,7 @@ public class OverviewDashboard {
     }
     return null;
   }
+
   public String getDashboardType() {
     return dashboardType;
   }
