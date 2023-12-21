884f6e6c980fcdf59064497bba83a07c31999fa6
eiquanasfiya <eiquanasfiya18@gmail.com>
2023-12-22 00:45:45 +0500

    Incorporate PR comments

--- src/main/java/org/owasp/dsomm/metricCA/analyzer/security/config/TokenConfiguration.java
+++ src/main/java/org/owasp/dsomm/metricCA/analyzer/security/config/TokenConfiguration.java
@@ -1,31 +1,59 @@
 package org.owasp.dsomm.metricCA.analyzer.security.config;
 
+import org.apache.logging.log4j.util.Strings;
+import org.owasp.dsomm.metricCA.analyzer.exception.SkeletonNotFoundException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 
+import javax.annotation.PostConstruct;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Properties;
 
 @Configuration
 public class TokenConfiguration {
 
   public static final String METRIC_CA_ACCESS_TOKEN_SECRET = "metricCA.access.token.secret";
+  public static final String TOKEN_NOT_FOUND = "Token must not be null";
+  public static final String SECRET_FILE_PATH_NOT_FOUND = "secretFilePath must not be null";
 
   @Value("${metricCA.access.token.secret.path}")
   private String secretFilePath;
+  @Value("${metricCA.access.token.enabled}")
+  private boolean enabled;
 
-  public String getToken() {
+  private String token;
+
+  @PostConstruct
+  public void builtToken() {
     Resource resource = new ClassPathResource(secretFilePath);
     try {
-      Properties properties = PropertiesLoaderUtils.loadProperties(resource);
-      return properties.get(METRIC_CA_ACCESS_TOKEN_SECRET).toString();
+      if (enabled) {
+        if (Strings.isNotEmpty(secretFilePath)) {
+          Properties properties = PropertiesLoaderUtils.loadProperties(resource);
+          String tokenValue = properties.get(METRIC_CA_ACCESS_TOKEN_SECRET).toString();
+          if (Strings.isNotEmpty(tokenValue)) {
+            token = tokenValue;
+          } else {
+            throw new SkeletonNotFoundException(TOKEN_NOT_FOUND);
+          }
+        } else {
+          throw new SkeletonNotFoundException(SECRET_FILE_PATH_NOT_FOUND);
+        }
+      }
+    } catch (FileNotFoundException cause) {
+      throw new SkeletonNotFoundException(cause.getMessage());
     } catch (IOException e) {
-      e.printStackTrace();
+      throw new RuntimeException(e);
     }
-    return null;
+  }
+
+  public String getToken(){
+    return token;
   }
 
 }
--- src/main/java/org/owasp/dsomm/metricCA/analyzer/security/filter/TokenFilter.java
+++ src/main/java/org/owasp/dsomm/metricCA/analyzer/security/filter/TokenFilter.java
@@ -3,9 +3,6 @@ package org.owasp.dsomm.metricCA.analyzer.security.filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.owasp.dsomm.metricCA.analyzer.security.config.TokenConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -14,16 +11,9 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Component
 public class TokenFilter extends OncePerRequestFilter {
 
-  @Autowired
-  TokenConfiguration tokenConfiguration;
-  @Value("${metricCA.access.token.enabled}")
-  private boolean enabled;
-
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
-    if (enabled) {
-      //TODO
-      System.out.println(tokenConfiguration.getToken());
-    }
+
   }
+
 }
