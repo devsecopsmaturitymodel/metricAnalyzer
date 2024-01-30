1dc98ab8a1a3c548b5ec9a5d5484e7bdfb49e9d4
Timo Pagel <github@timo-pagel.de>
2024-01-30 10:22:23 +0100

    fix: tests

--- src/test/java/org/owasp/dsomm/metricca/analyzer/deserialization/ApplicationDirectorTest.java
+++ src/test/java/org/owasp/dsomm/metricca/analyzer/deserialization/ApplicationDirectorTest.java
@@ -1,69 +1,42 @@
-//package org.owasp.dsomm.metricca.analyzer.deserialization;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//import org.owasp.dsomm.metricca.analyzer.deserialization.activity.Activity;
-//
-//import java.lang.reflect.Constructor;
-//import java.lang.reflect.Field;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//public class ApplicationDirectorTest {
-//
-//  private ApplicationDirector applicationDirector;
-//
-//  @Mock
-//  private YamlScanner yamlScanner;
-//
-//  @BeforeEach
-//  public void setup() {
-//    MockitoAnnotations.openMocks(this);
-//  }
-//
-//  @BeforeEach
-//  public void setUp() throws Exception {
-//    Constructor<ApplicationDirector> constructor = ApplicationDirector.class.getDeclaredConstructor();
-//    constructor.setAccessible(true);
-//    applicationDirector = constructor.newInstance();
-//    YamlScanner yamlScanner = new YamlScanner();
-//    setPrivateField(yamlScanner, "yamlApplicationFolderPath", "src/test/resources/test-all/definitions");
-//    setPrivateField(yamlScanner, "yamlSkeletonFilePath", "src/test/resources/test-all/skeleton.yaml");
-//    yamlScanner.getApplicationYamls();
-//    setPrivateField(this.applicationDirector, "yamlScanner", yamlScanner);
-//  }
-//
-//  private void setPrivateField(Object targetObject, String fieldName, Object valueToSet) {
-//    try {
-//      Field field = targetObject.getClass().getDeclaredField(fieldName);
-//      field.setAccessible(true);
-//      field.set(targetObject, valueToSet);
-//    } catch (NoSuchFieldException | IllegalAccessException e) {
-//      throw new RuntimeException(e);
-//    }
-//  }
-//
-//  @Test
-//  public void testApplicationsExists() throws Exception {
-//    List<Application> actualApplications = applicationDirector.getApplications();
-//    assertTrue(actualApplications.size() > 0);
-//  }
-//
-//  @Test
-//  public void testTeamActivitySecurityRequirementsExists() throws Exception {
-//    List<Application> actualApplications = applicationDirector.getApplications();
-//    boolean foundSecurityRequirements = false;
-//    for (Application application : actualApplications) {
-//      for (Activity activity : application.getActivities()) {
-//        if (activity.getName().equals("Security requirements")) {
-//          foundSecurityRequirements = true;
-//          break;
-//        }
-//      }
-//    }
-//    assertTrue(foundSecurityRequirements);
-//  }
-//}
+package org.owasp.dsomm.metricca.analyzer.deserialization;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.owasp.dsomm.metricca.analyzer.ApplicationTest;
+import org.owasp.dsomm.metricca.analyzer.deserialization.activity.Activity;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ApplicationDirectorTest extends ApplicationTest {
+  @BeforeEach
+  public void setup() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  public void testApplicationsExists() throws Exception {
+    List<Application> actualApplications = applicationDirector.getApplications();
+    assertTrue(actualApplications.size() > 0);
+  }
+
+  @Test
+  public void testTeamActivitySecurityRequirementsExists() throws Exception {
+    List<Application> actualApplications = applicationDirector.getApplications();
+    boolean foundSecurityRequirements = false;
+    for (Application application : actualApplications) {
+      for (Activity activity : application.getActivities()) {
+        if (activity.getName().equals("Security requirements")) {
+          foundSecurityRequirements = true;
+          break;
+        }
+      }
+    }
+    assertTrue(foundSecurityRequirements);
+  }
+}
--- src/test/java/org/owasp/dsomm/metricca/analyzer/deserialization/DatePeriodWithSecurityRequirementsTest.java
+++ src/test/java/org/owasp/dsomm/metricca/analyzer/deserialization/DatePeriodWithSecurityRequirementsTest.java
@@ -6,6 +6,7 @@ import org.eclipse.jgit.api.errors.GitAPIException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
+import org.owasp.dsomm.metricca.analyzer.ApplicationTest;
 import org.owasp.dsomm.metricca.analyzer.deserialization.activity.UrlActivity;
 import org.owasp.dsomm.metricca.analyzer.deserialization.activity.threshold.DatePeriod;
 import org.slf4j.Logger;
@@ -22,39 +23,8 @@ import java.util.TimeZone;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class DatePeriodWithSecurityRequirementsTest {
+public class DatePeriodWithSecurityRequirementsTest extends ApplicationTest {
   private static final Logger logger = LoggerFactory.getLogger(DatePeriodWithSecurityRequirementsTest.class);
-
-  private ApplicationDirector applicationDirector;
-
-  @BeforeEach
-  public void setup() {
-    MockitoAnnotations.openMocks(this);
-  }
-
-  @BeforeEach
-  public void setUp() throws Exception {
-    Constructor<ApplicationDirector> constructor = ApplicationDirector.class.getDeclaredConstructor();
-    constructor.setAccessible(true);
-    applicationDirector = constructor.newInstance();
-    YamlScanner yamlScanner = new YamlScanner();
-    yamlScanner.initiate();
-    setPrivateField(yamlScanner, "yamlApplicationFolderPath", "src/test/resources/test-security-requirements/definitions");
-    setPrivateField(yamlScanner, "yamlSkeletonFilePath", "src/test/resources/test-security-requirements/skeleton.yaml");
-    yamlScanner.getApplicationYamls();
-    setPrivateField(this.applicationDirector, "yamlScanner", yamlScanner);
-    yamlScanner.getSkeletonYaml();
-  }
-
-  private void setPrivateField(Object targetObject, String fieldName, Object valueToSet) {
-    try {
-      Field field = targetObject.getClass().getDeclaredField(fieldName);
-      field.setAccessible(true);
-      field.set(targetObject, valueToSet);
-    } catch (NoSuchFieldException | IllegalAccessException e) {
-      throw new RuntimeException(e);
-    }
-  }
 
   private UrlActivity getSecurityRequirementsActivity() throws GitAPIException, IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
     List<Application> actualApplications = applicationDirector.getApplications();
@@ -104,7 +74,7 @@ public class DatePeriodWithSecurityRequirementsTest {
     SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd");
     isoFormat.setTimeZone(TimeZone.getTimeZone("Etc/UTC"));
 
-    Date existingDate = isoFormat.parse("2021-05-01");
+    Date existingDate = isoFormat.parse("2000-01-01");
     DatePeriod endDateForExistingDate = activity.getThresholdDatePeriodMap().get("Level 1").getDatePeriodForDate(existingDate);
     assertNotNull(endDateForExistingDate);
     assertTrue(endDateForExistingDate.getShowEndDate());
